1a3508454a7650562a7baa9e40a6636e8f82f570
IshankaDinusha <166381146+IshankaDinusha@users.noreply.github.com>
2025-05-04 00:59:06 +0530

    - Navbar update
    - Added sign-up and log-in
    
    - Update cards
    
    Fixed responsiveness
    
    Carousel update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const errorModal = createModal("errorModal");
 const trailerModal = createModal("trailerModal");
+const logInModal = createModal("logInModal");
 
 function createModal(id) {
 
@@ -90,7 +91,134 @@ function hideTrailerModal() {
 
 }
 
-/*--Login Nav Function--*/
-document.getElementById("logInBtn").addEventListener("click", function () {
-    window.location.href = "login.html"; // Change this to your target page
-});
+function toggleIcon(element) {
+
+    element.classList.toggle("open");
+}
+
+
+
+function signUpConfirm(element, isConfirm) {
+
+    if (isConfirm) {
+
+        element.style.pointerEvents = "none"; // prevent multiple clicks
+        element.parentElement.classList.add("da-slide-left");
+
+        trackSignUpProgress(true);
+
+        setTimeout(function () {
+            element.style.pointerEvents = "auto";
+            element.parentElement.classList.add("d-none");
+            element.parentElement.classList.remove("da-slide-left");
+            element.parentElement.nextElementSibling.classList.add("da-slide-right");
+            element.parentElement.nextElementSibling.classList.remove("d-none");
+
+            setTimeout(function () {
+                element.parentElement.nextElementSibling.classList.toggle("da-slide-show");
+
+                setTimeout(function () {
+                    element.parentElement.nextElementSibling.classList.remove("da-slide-right");
+                    element.parentElement.nextElementSibling.classList.remove("da-slide-show");
+                }, 300);
+
+            }, 10);
+
+        }, 300);
+
+    } else {
+
+        element.style.pointerEvents = "none"; // prevent multiple clicks
+        element.parentElement.classList.add("da-slide-right");
+
+        trackSignUpProgress(false);
+
+        setTimeout(function () {
+            element.style.pointerEvents = "auto";
+            element.parentElement.classList.add("d-none");
+            element.parentElement.classList.remove("da-slide-right");
+            element.parentElement.previousElementSibling.classList.add("da-slide-left");
+            element.parentElement.previousElementSibling.classList.remove("d-none");
+
+            setTimeout(function () {
+                element.parentElement.previousElementSibling.classList.toggle("da-slide-show");
+
+                setTimeout(function () {
+                    element.parentElement.previousElementSibling.classList.remove("da-slide-left");
+                    element.parentElement.previousElementSibling.classList.remove("da-slide-show");
+                }, 300);
+
+            }, 10);
+
+        }, 300);
+    }
+}
+
+var signUpProgress = 0;
+
+function trackSignUpProgress(isConfirm) {
+
+    if (isConfirm) {
+
+        signUpProgress += 1;
+    } else {
+
+        signUpProgress -= 1;
+    }
+
+    document.getElementById("signUpProgressBars").className = "progress-" + signUpProgress.toString();
+
+    if (signUpProgress == 3) {
+
+        document.getElementById("signUpFooter").style.visibility = "hidden";
+        document.getElementById("logInFooter").style.visibility = "hidden";
+        document.getElementById("signUpHeader").classList.remove("sign-up-header-show");
+
+        setTimeout(function () {
+            document.getElementById("signUpHeaderTitle").innerHTML = "Sign Up Completed";
+            document.getElementById("signUpHeader").classList.add("sign-up-header-show");
+        }, 300);
+    }
+}
+
+
+function changeSignUpView(isLogin) {
+
+    const signUpView = document.getElementById("signUpView");
+    const logInView = document.getElementById("logInView");
+
+    if (isLogin) {
+
+        signUpView.classList.remove("da-show");
+
+        setTimeout(function () {
+
+            signUpView.classList.add("d-none");
+            logInView.classList.remove("d-none");
+            
+            setTimeout(function () {
+
+                logInView.classList.add("da-show");
+            }, 10);
+
+
+        }, 300);
+
+    } else {
+
+        logInView.classList.remove("da-show");
+
+        setTimeout(function () {
+
+            logInView.classList.add("d-none");
+            signUpView.classList.remove("d-none");
+
+            setTimeout(function () {
+
+                signUpView.classList.add("da-show");
+            }, 10);
+
+        }, 300);
+
+    }
+}
